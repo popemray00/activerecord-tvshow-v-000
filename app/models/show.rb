1bc6d3713964ab0e225762c17341c1ee490ceba3
Michael Pope <popemray1986@gmail.com>
2018-03-09 05:18:59 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,12 @@
 class Show < ActiveRecord::Base
+
   def self.highest_rating
     Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    show = Show.find_by(rating: highest_rating)
+    show
+
   end
 end
